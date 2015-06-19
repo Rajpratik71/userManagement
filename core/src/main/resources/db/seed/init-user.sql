59fcdeaef94c9fd223668210e032d62344443fe8
chenbojian <chenbojian@foxmail.com>
2015-06-19 16:28:28 +0800

    change seed sql file to make password encrypt via md5

--- core/src/main/resources/db/seed/init-user.sql
+++ core/src/main/resources/db/seed/init-user.sql
@@ -1,6 +1,6 @@
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Tom', 'password','tom@abc.com', 16);
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Jerry', 'password','jerry@abc.com', 17);
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Red', 'password','red@abc.com', 18);
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Marry', 'password','marry@abc.com', 19);
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('James', 'password','james@abc.com', 20);
-insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Emma', 'password','emma@abc.com', 21);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Tom', '5f4dcc3b5aa765d61d8327deb882cf99','tom@abc.com', 16);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Jerry', '5f4dcc3b5aa765d61d8327deb882cf99','jerry@abc.com', 17);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Red', '5f4dcc3b5aa765d61d8327deb882cf99','red@abc.com', 18);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Marry', '5f4dcc3b5aa765d61d8327deb882cf99','marry@abc.com', 19);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('James', '5f4dcc3b5aa765d61d8327deb882cf99','james@abc.com', 20);
+insert into USER ( NAME, PASSWORD, EMAIL, AGE) values ('Emma', '5f4dcc3b5aa765d61d8327deb882cf99','emma@abc.com', 21);

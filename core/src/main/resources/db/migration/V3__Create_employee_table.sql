b4a9db5619fb2b05c2cb72e6ad7fcc496b495b1f
chenbojian <chenbojian@foxmail.com>
2015-06-23 14:14:02 +0800

    change POSITION to JOB to make meaning more understandable

--- core/src/main/resources/db/migration/V3__Create_employee_table.sql
+++ core/src/main/resources/db/migration/V3__Create_employee_table.sql
@@ -1,6 +1,6 @@
 create table EMPLOYEE (
     USER_ID int not null,
-    POSITION varchar(100) not null,
+    JOB varchar(100) not null,
     UNIQUE (USER_ID),
     FOREIGN KEY (USER_ID) REFERENCES USER(ID)
 );
